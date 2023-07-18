3a5b7d18fb3c9d3274da638537beb81c49510766
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-07-18 14:25:53 +0200

    add js and md text

---
+++ file.md
@@ -0,0 +1,6 @@
+# Chiedere all'utente informazioni personali , pubblico il risultato finale sulla pagina
+
+- chiedo all'utente il suo nome
+- chiedo all'utente il suo cognome
+- chiedo all'utente il suo colore preferito
+- risultato finale visualizzata su pagina con tutti i dati inseriti dall'utente
---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Chiedi all'utente il suo nome
+let nome = prompt("Inserisci il tuo nome:");
+
+// Chiedi all'utente il suo cognome
+let cognome = prompt("Inserisci il tuo cognome:");
+
+// Chiedi all'utente il suo colore preferito
+let colorePreferito = prompt("Inserisci il tuo colore preferito:");
+
+// Componi la stringa finale
+let risultato = nome + cognome + colorePreferito + "21";
+
+// Scrivi il risultato sulla pagina
+document.write(risultato);
